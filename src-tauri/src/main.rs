c5e3ad24c4c9483b7808b9c12fb4f45a45652f34
fxredeemer <flufeu@gmail.com>
2021-02-26 18:18:56 +0100

    testing irc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,8 +6,13 @@
 mod cmd;
 
 use cmd::Cmd::*;
+use futures::executor::block_on;
+use futures::prelude::*;
+use irc::client::prelude::*;
 
 fn main() {
+    block_on(start_chat_async());
+
     tauri::AppBuilder::new()
         .invoke_handler(|_webview, arg| match serde_json::from_str(arg) {
             Err(e) => Err(e.to_string()),
@@ -18,6 +23,23 @@ fn main() {
         })
         .build()
         .run();
+}
+
+async fn start_chat_async() {
+    // configuration is loaded from config.toml into a Config
+    let mut client = Client::new("../config.toml").await.unwrap();
+
+    // identify comes from ClientExt
+    client.identify().unwrap();
+    let mut stream = client.stream().unwrap();
+    while let Some(message) = stream.next().await.transpose().unwrap() {
+        if let Command::PRIVMSG(channel, message) = message.command {
+            if message.contains(&*client.current_nickname()) {
+                // send_privmsg comes from ClientExt
+                client.send_privmsg(&channel, "beep boop").unwrap();
+            }
+        }
+    }
 }
 
 fn execute_command(_webview: &mut tauri::Webview, command: cmd::Cmd) -> () {
