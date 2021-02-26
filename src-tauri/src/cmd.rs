7367c33382ad3ee0cfcf949661c850f1dad24398
fxredeemer <flufeu@gmail.com>
2021-02-26 17:45:27 +0100

    changed to 4 spaces / tab

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -3,13 +3,13 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 #[serde(tag = "cmd", rename_all = "camelCase")]
 pub enum Cmd {
-  Connect {
-    address: String,
-    callback: String,
-    error: String,  
-  },
-  GetPortInfo {
-    callback: String,
-    error: String,
-  },
+    Connect {
+        address: String,
+        callback: String,
+        error: String,
+    },
+    GetPortInfo {
+        callback: String,
+        error: String,
+    },
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 mod cmd;
@@ -8,38 +8,38 @@ mod cmd;
 use cmd::Cmd::*;
 
 fn main() {
-  tauri::AppBuilder::new()
-    .invoke_handler(|_webview, arg| match serde_json::from_str(arg) {
-      Err(e) => Err(e.to_string()),
-      Ok(command) => {
-        execute_command(_webview, command);
-        Ok(())
-      }
-    })
-    .build()
-    .run();
+    tauri::AppBuilder::new()
+        .invoke_handler(|_webview, arg| match serde_json::from_str(arg) {
+            Err(e) => Err(e.to_string()),
+            Ok(command) => {
+                execute_command(_webview, command);
+                Ok(())
+            }
+        })
+        .build()
+        .run();
 }
 
 fn execute_command(_webview: &mut tauri::Webview, command: cmd::Cmd) -> () {
-  match command {
-    Connect {
-      address,
-      callback,
-      error,
-    } => tauri::execute_promise(
-      _webview,
-      move || {
-        println!("{}", address);
-        Ok("Success")
-      },
-      callback,
-      error,
-    ),
-    GetPortInfo { callback, error } => tauri::execute_promise(
-      _webview,
-      move || Ok("{ key: 'response', value: [{ id: 3 }] }".to_string()),
-      callback,
-      error,
-    ),
-  }
+    match command {
+        Connect {
+            address,
+            callback,
+            error,
+        } => tauri::execute_promise(
+            _webview,
+            move || {
+                println!("{}", address);
+                Ok("Success")
+            },
+            callback,
+            error,
+        ),
+        GetPortInfo { callback, error } => tauri::execute_promise(
+            _webview,
+            move || Ok("{ key: 'response', value: [{ id: 3 }] }".to_string()),
+            callback,
+            error,
+        ),
+    }
 }
